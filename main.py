1e9c04bebdfdfa3f0d75bf0db2b6f7c69287a0fa
Jens Kromdijk <jenskromdijk@gmail.com>
2026-02-26 12:01:35 +0000

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Basic pygame template that runs on the web using pygbag (also still works on desktop).
 # Setup:
 # pip install pygame-ce pygbag
-# 
+#
 # Running (from root directory):
 # pygbag .
 
@@ -15,7 +15,8 @@ pygame.mixer.init()
 WEB_PLATFORM = sys.platform == "emscripten"
 if WEB_PLATFORM:
     # for document/canvas interaction
-    import js # type: ignore
+    import js  # type: ignore
+
     # keep pixelated look for pygbag
     platform.window.canvas.style.imageRendering = "pixelated"
 
@@ -28,14 +29,14 @@ class App:
         # no need for separate scaling, pygbag scales canvas automatically
         self.display = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.RESIZABLE)
         self.screen = pygame.Surface((WIDTH // SCALE, HEIGHT // SCALE))
-        self.active = True # if tab is focused when running through web
+        self.active = True  # if tab is focused when running through web
 
         self.clock = pygame.time.Clock()
 
         # delta time
         self.dt = 1
-        self.last_time = time.time() - 1/60
-    
+        self.last_time = time.time() - 1 / 60
+
     # put all the game stuff here
     def update(self):
         # update delta time
@@ -56,7 +57,7 @@ class App:
                 # handle window resizing on desktop
                 if event.type == pygame.WINDOWRESIZED:
                     self.screen = pygame.Surface((self.display.get_width() // SCALE, self.display.get_height() // SCALE))
-            
+
             # update game
             self.update()
 
@@ -70,8 +71,10 @@ class App:
                 if WEB_PLATFORM:
                     pygame.display.set_caption(f"FPS: {self.clock.get_fps() :.1f}")
                 else:
-                    pygame.display.set_caption(f"FPS: {self.clock.get_fps() :.1f} Display: {self.screen.get_width()} * {self.screen.get_height()}")
-                
+                    pygame.display.set_caption(
+                        f"FPS: {self.clock.get_fps() :.1f} Display: {self.screen.get_width()} * {self.screen.get_height()}"
+                    )
+
                 # scale display (don't use scale2x, which uses pixel nearest algorithm and ruins pixel art)
                 self.display.blit(pygame.transform.scale(self.screen, self.display.get_size()), (0, 0))
                 pygame.display.flip()
@@ -79,13 +82,15 @@ class App:
                 # if browser tab isn't focused, don't update the display and just change the caption to 'IDLE'
                 pygame.display.set_caption("IDLE")
 
-            await asyncio.sleep(0) # IMPORTANT: keep this for pygbag to work!
-            self.clock.tick(60) # don't really need more than 60 fps
+            await asyncio.sleep(0)  # IMPORTANT: keep this for pygbag to work!
+            self.clock.tick(60)  # don't really need more than 60 fps
+
 
 # run App() asynchronously so it works with pygbag
 async def main():
     app = App()
     await app.run()
+
 
 # start
 asyncio.run(main())
--- src/util.py
+++ src/util.py
@@ -1,21 +1,25 @@
 import pygame, os, json
 
-BASE_IMG_PATH = 'data/images/'
-BASE_AUDIO_PATH = 'data/audio/'
+BASE_IMG_PATH = "data/images/"
+BASE_AUDIO_PATH = "data/audio/"
+
 
 def load_image(path) -> pygame.Surface:
     surf = pygame.image.load(BASE_IMG_PATH + path).convert()
     surf.set_colorkey((0, 0, 0))
     return surf
 
+
 def load_images(path):
     imgs = []
     for img_path in os.listdir(BASE_IMG_PATH + path):
-        imgs.append(load_image(path + '/' + img_path))
+        imgs.append(load_image(path + "/" + img_path))
     return imgs
+
 
 def load_sound(path) -> pygame.mixer.Sound:
     return pygame.mixer.Sound(BASE_AUDIO_PATH + path)
+
 
 def load_tile_imgs(path, tile_size):
     img = load_image(path)
@@ -31,21 +35,25 @@ def load_tile_imgs(path, tile_size):
             tiles.append(img_surf.copy())
     return tiles
 
+
 def snip(spritesheet, pos, dimensions):
     clip_rect = pygame.Rect(pos, dimensions)
     image = spritesheet.subsurface(clip_rect)
     return image
 
+
 def read_json(path):
-    f = open(path, 'r')
+    f = open(path, "r")
     data = json.load(f)
     f.close()
     return data
 
+
 def write_json(path, data):
-    f = open(path, 'w')
+    f = open(path, "w")
     json.dump(data, f)
     f.close()
+
 
 def load_palette(img: pygame.Surface):
     img_array = pygame.pixelarray.PixelArray(img)
@@ -53,5 +61,6 @@ def load_palette(img: pygame.Surface):
     for row in img_array:
         for color in row:
             c = img.unmap_rgb(color)
-            if c != (0, 0, 0, 0): palette.append(tuple(c))
+            if c != (0, 0, 0, 0):
+                palette.append(tuple(c))
     return palette
